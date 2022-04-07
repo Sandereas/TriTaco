8ffad7e416f940e4007c8172f3833f428db3d82e
Michsow <94047110+Michsow@users.noreply.github.com>
2022-04-07 11:18:58 +0200

    Update Form.php

--- phpfiles/FormFiles/Form.php
+++ phpfiles/FormFiles/Form.php
@@ -1,5 +1,5 @@
 
-<link href="cssform.css" rel="stylesheet">
+<link href="cssform.css" rel="stylesheetForm">
 
   
 
@@ -10,7 +10,7 @@
   <form class="modal-content animate" action="/action_page.php" method="post">
     <div class="imgcontainer">
       <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="../img/avatar.jpg" alt="Avatar" class="avatar">
+      <img src="img/avatar.jpg" alt="Avatar" class="avatar">
     </div>
 
     <div class="container">
