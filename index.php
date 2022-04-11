9a133c4de33bcd63c3dba4c00c520aba7261056e
Michsow <94047110+Michsow@users.noreply.github.com>
2022-04-11 13:45:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,12 @@
     <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
       <!-- Bootstrap core CSS -->
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+      
     <link rel="stylesheet" href="css/main.css"> 
 
     <title>Restaurant</title>
-
+    <link href="phpfiles/FormFiles/cssform.css" rel="stylesheetForm">
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/album/">
-    
-  
 
   </head>
 
@@ -77,5 +75,3 @@ include "phpfiles/connection.php";
   include "phpfiles/footer.php";
 ?>
     
-
-<script scr="JS/main.js">
