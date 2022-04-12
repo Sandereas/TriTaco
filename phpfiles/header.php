90060a13529911ffa40b5262e3357a07755d1b43
Michsow <94047110+Michsow@users.noreply.github.com>
2022-04-12 11:19:02 +0200

    Update header.php

--- phpfiles/header.php
+++ phpfiles/header.php
@@ -25,7 +25,6 @@
             <label for="fname">Search:</label>
             <input type="text" id="fname" name="fname"><br><br>
           </form>
-
           <a href="#" class="navbar-brand d-flex align-items-center">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
             <strong>Restaurant</strong>
@@ -37,7 +36,7 @@
               <a href="#Wiki">Wiki</a>
               <a href="#contact">Contact</a>
               <a href="#about">About</a>
-              
+              <a href="Users.php">Users</a>
             </div>
         </div>
       </div>
