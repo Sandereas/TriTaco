db6e3c20f95e2451f4fe87658844a272588c63b8
Michsow <94047110+Michsow@users.noreply.github.com>
2022-04-12 11:40:24 +0200

    Update Users.php

--- Users.php
+++ Users.php
@@ -1,15 +1,31 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="css/main.css"> 
+</head>
+<body>
+    
 <?php
 include "phpfiles/connection.php"; ?>
 <div class="row"> <?php
 $data = $conn->query("SELECT * FROM users")->fetchAll();
 
 foreach ($data as $row) { ?>
-    <table class="div1Users">
+    <table class="Table1Users">
         <tr>
-            <th>    <?php echo $row['UserName']."<br />\n";?> </th>
-            <th>    <?php echo $row['UserID']."<br />\n"; ?>  </th>
+            <th> Username </th>
+            <th> UserID  </th>
         </tr>
+        <tr>
+            <td> <?php echo $row['UserName']."<br />\n";?>  </td>
+            <td> <?php echo $row['UserID']."<br />\n"; ?>   </td>
+        </tr>
+
     </table>
 </div>
 <?php } ?>
@@ -18,3 +34,5 @@ foreach ($data as $row) { ?>
 <?php
 include "phpfiles/footer.php"
 ?>
+</body>
+</html>
