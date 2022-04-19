218ac5135ba31cd32a562639e67514720d3eccce
Michsow <94047110+Michsow@users.noreply.github.com>
2022-04-19 10:32:59 +0200

    Update Edit.php

--- phpfiles/Edit.php
+++ phpfiles/Edit.php
@@ -1,23 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/main.css" rel="stylesheet">
+  <title>Document</title>
+</head>
+<body>
+  
+
+
+
 <?php
 
-var_dump($_GET);
-
-include "phpfiles/connection.php";
+include "connection.php";
 $data = $conn->query("SELECT * FROM album WHERE ID=".$_GET['p'])->fetch();
 var_dump($data['ID']);
-
-
-
-$data = [
-    'ID' => $ID,
-    'titel' => $titel,
-    'Text_Details' => $Text_Details,
-    'Ingredienten' => $Ingredienten,
-    'prijs' => $prijs,
-];
-$sql = "UPDATE users SET name=:name, surname=:surname, sex=:sex WHERE id=:id";
-$stmt= $pdo->prepare($sql);
-$stmt->execute($data);
-
-
+//foreach ($data as $row) {
+  //echo $row ['titel']. " " .$row['artiest']. "<br >/\n";
+//}
+include "admin1.php"
 ?>
+
+
+<div class=imgholder >
+    <img src="img/<?php echo $data ['photo'];?>">
+</div>
+<div class="FirstTextHolder">
+  <p>  <?php echo $data['Text_Details'] ?>  <p>
+</div>
+<div class="SecondTextHolder">
+
+</div>
+<div>
+  <p>  <?php echo $data['Ingredienten'] ?>  <p> 
+</div>
+<div>
+
+</div>
+
+
+
+
+
+</body>
+</html>
