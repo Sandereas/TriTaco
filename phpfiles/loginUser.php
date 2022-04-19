3d7314dd5a8450ac706004f7fc44d90b41f822b5
Michsow <94047110+Michsow@users.noreply.github.com>
2022-04-19 20:43:32 +0200

    Update loginUser.php

--- phpfiles/loginUser.php
+++ phpfiles/loginUser.php
@@ -1,5 +1,5 @@
 <?php 
-
+ob_start();
 include "connection.php";
 
 $sql = "SELECT * FROM users WHERE UserName = :UserName AND UserPassword =:UserPassword";
@@ -12,8 +12,21 @@ $result = $stmt->fetchAll();
 $username = $_POST['UserName'];
 
 if(count($result) > 0){
+    
+session_start();
     $_SESSION["UserName"] = $username;
-    header('Location: admin1.php');
+    
+    if(isset($_SESSION["UserName"])){
+        
+        echo  $_SESSION["UserName"];
+
+        header('Location: ../index.php');
+        ob_get_contents();
+        exit();        
+        
+
+    }
+    
 } else{
     echo "username niet gevonden";
 }
