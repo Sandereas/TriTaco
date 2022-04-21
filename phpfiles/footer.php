6382b6647350e7be2ec52fdce94e4479b59e6c0f
Michsow <94047110+Michsow@users.noreply.github.com>
2022-04-21 08:55:22 +0200

    Update footer.php

--- phpfiles/footer.php
+++ phpfiles/footer.php
@@ -9,6 +9,7 @@
         <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
         <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
       </div>
+       
     </footer>
 
     <!-- Bootstrap core JavaScript
@@ -16,5 +17,6 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script scr="JS/main.js">
+     
   </body>
 </html>
