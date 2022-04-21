71df0a0da871d85f3990a038fe4585834789e641
Michsow <94047110+Michsow@users.noreply.github.com>
2022-04-21 08:55:33 +0200

    Update Register.php

--- phpfiles/Register.php
+++ phpfiles/Register.php
@@ -69,37 +69,43 @@ a {
 <body>
 <?php
 include ("connection.php");
-$sth = $conn->prepare("INSERT INTO users (UserID, UserName, E-mail, Password ) 
-                        VALUES (:Username, :email, UserPassword, psw-repeat)");
+var_dump($_POST);
+if(isset($_POST['submit'])){
 
-$sth->bindParam('Username', $_POST['Username']);
-$sth->bindParam('email', $_POST['email']);
-$sth->bindParam('UserPassword', $_POST['UserPassword']);
-$sth->bindParam('psw-repeat', $_POST['psw-repeat']);
-$sth->execute();
-$result = $stmt->fethAll();
+
+    $sth = $conn->prepare("INSERT INTO users (UserName, E_mail, UserPassword ) 
+                            VALUES (:UserName, :Email, :UserPassword )");
+
+    $sth->bindParam(':UserName', $_POST['UserName']);
+    $sth->bindParam(':Email', $_POST['email']);
+    $sth->bindParam(':UserPassword', $_POST['UserPassword']);
+    $sth->execute();
+    //$sth->debugDumpParams();
+    $result = $sth->fetch();
+    header("Location: ../index.php");
+}
 ?>
-<form action="Users.php" method="post">
+<form action="" method="post">
   <div class="container">
     <h1>Register</h1>
     <p>Please fill in this form to create an account.</p>
     <hr>
 
     <label for="email"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="Username" id="Username" required>
+    <input type="text" placeholder="Enter Username" name="UserName" id="Username" required>
 
     <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" name="email" id="email" required>
 
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+    <input type="password" placeholder="Enter Password" name="UserPassword" id="psw" required>
 
     <label for="psw-repeat"><b>Repeat Password</b></label>
     <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
     <hr>
     <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
 
-    <button type="submit" class="registerbtn">Register</button>
+    <button type="submit" class="registerbtn" name="submit">Register</button>
   </div>
   
 
