ef721e126ffeab457a5c5e15301ab35c665dba1f
Michsow <94047110+Michsow@users.noreply.github.com>
2022-04-21 10:19:25 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -5,4 +5,22 @@ function openForm() {
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
-  
+
+
+const FormElement = document.querySelector('#LogInForm');
+
+  FormElement.addEventListener('submit', (e) => {
+
+      const username = document.querySelector('#UserName').value;
+      const password = document.querySelector('#UserPassword').value;
+
+      if (password == '' || username == '') {
+
+          e.preventDefault();
+          alert('Please enter a username and password');
+      }
+
+      else{
+      }
+
+  });
