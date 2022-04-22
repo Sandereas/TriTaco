a1636ace34366d20f380a20f4bd7776cf6d559b2
Michsow <94047110+Michsow@users.noreply.github.com>
2022-04-22 14:56:50 +0200

    Update footer.php

--- phpfiles/footer.php
+++ phpfiles/footer.php
@@ -16,7 +16,9 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script scr="JS/main.js">
+    <script scr="JS/main.js"></script>
+    <script src="JS/JavaForm.js"> </script>
+
      
   </body>
 </html>
