0b0701b2deca547889499ff3c51f768eb367ce1b
Michsow <94047110+Michsow@users.noreply.github.com>
2022-04-22 17:18:49 +0200

    Update admin1.php

--- phpfiles/admin1.php
+++ phpfiles/admin1.php
@@ -1,4 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/admin1.css">
+    <title>Document</title>
+</head>
+<body>
+    
 <?php
 include ('connection.php');
 if(isset($_SESSION['UserName'])) {
@@ -18,13 +28,16 @@ $result = $stmt->fetchAll();
  { ?><br>
 
 <tr class="table1">
-<td><?php echo $data["titel"]; ?> </td> <br>
-<td><?php echo $data["Text_Details"]; ?> </td> <br>
-<td><?php echo $data["Ingredienten"]; ?> </td> <br>
-<td><?php echo $data["prijs"]; ?> </td> <br>
+<td class="row1"><?php echo $data["titel"]; ?> </td> <br>
+<td class="row2"><?php echo $data["Text_Details"]; ?> </td> <br>
+<td class="row3"><?php echo $data["Ingredienten"]; ?> </td> <br>
+<td class="row4"><?php echo $data["prijs"]; ?> </td> <br>
 </tr>
 <a href="editBTN.php?id=<?php echo $data["ID"]; ?>">edit</a>
 <a href="delete.php?id=<?php echo $data["ID"]; ?>">delete</a> <br>
 <?php 
 }
 ?>
+
+</body>
+</html>
