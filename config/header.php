32bcd8743e06615f491954a01c234bddca04d3a7
keyanl <1192274@student.roc-nijmegen.nl>
2022-06-29 09:53:03 +0200

    kleine aanpassingen

--- config/header.php
+++ config/header.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUDSandervdv</title>
+    <title>CRUDSANDERKEYAN</title>
     <link rel="stylesheet" href="../css/home.css">
     
 
@@ -21,7 +21,9 @@
             
             <ul>
                 <li> <?php if($_SESSION['loggedin'] == true){
-                echo $_SESSION['username']; } ?> </li>
+                echo $_SESSION['username']; } else {
+                    return; 
+                } ?> </li>
                 <br> <br>
                 <li> <a href= "../site/home.php"> Home </a> </li>
                 <li> <a href= "../php/destinations.php"> Destinations </a> </li>
