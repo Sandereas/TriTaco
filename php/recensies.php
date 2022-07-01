92b0502b8868219fe6048d554c9f84eff49f831a
keyanl <1192274@student.roc-nijmegen.nl>
2022-07-01 11:04:56 +0200

    recensies 2

--- php/recensies.php
+++ php/recensies.php
@@ -1,4 +1,77 @@
 <?php include('../config/header.php'); ?>
+<?php include ('../config/recensiesconnect.php'); ?>
+
+<?php
+ if($_SESSION['loggedin'] == true){
+     echo "GG ". $_SESSION['username'];
+ }
+ else {
+         header("Location: login.php");
+         
+     }
+     
+?>
+
+<?php
+    //Add Recensies
+    if(isset($_POST['submit']))
+{
+    $recensieID = $_POST ['recensieID'];
+    $reismogelijkheidID = ($_POST ['reismogelijkheidID']);
+    $userID = $_POST ['userID'];
+    $gevalideerd = $_POST ['gevalideerd'];
+    $stars = $_POST ['stars'];
+    $date = $_POST ['date'];
+    $message = $_POST ['message'];
+
+
+    $sql = "INSERT INTO recensies SET
+        recensieID = recensieID,
+        reismogelijkheidID = :reismogelijkheidID,
+        userID = :userID,
+        gevalideerd =:gevalideerd,
+        stars = :stars,
+        date = :date,
+        message = :message
+    ";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+    'recensieID' => $recensieID,
+    'reismogelijkheidID' => $reismogelijkheidID,
+    'userID' => $userID,
+    'gevalideerd' => $gevalideerd,
+    'stars' => $stars,
+    'date' => $date,
+    'message' => $message
+
+
+]); 
+$recensies = $stmt->fetch();
+    
+    // uitvoeren en opslaan in de database
+
+
+
+        if ($recensies > 0) {
+                $results_recensies = $stmt->fetch(PDO::FETCH_ASSOC);
+  
+        //echo "data inserted";
+        $_SESSION['add'] = "Uw feedback is achtergelaten!";
+        header('Location:../site/home.php');
+        }
+    else {
+        //echo "error";
+        $_SESSION['add'] = "Er is iets mis gegaan.";
+        header('Location:recensies.php');
+    }
+}
+    ?>
+
+
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,34 +79,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Feedback</title>
 </head>
 <body>
 <div class="form_box shadow">
- <form method="post" action="practice_ac.php">
+ <form method="post" action="recensiesconnect">
  <div class="heading">
    Feedback Form in PHP
  </div>
  <br/>
- <p>What do you think about the quality of our blog?</p>
+ <p>Hoe goed heeft onze website u geholpen?</p>
  <div>
-   <div class="pic">
-     <img src="bad.png" alt=""> <br/>
+   <div class="stars">
+     <img src="" alt=""> <br/>
      <input type="radio" name="quality" value="0"> Bad
    </div>
-   <div class="pic">
-     <img src="neutral.png" alt=""> <br/>
+   <div class="stars">
+     <img src="" alt=""> <br/>
      <input type="radio" name="quality" value="1"> Okay
    </div>
-   <div class="pic">
-     <img src="good.png" alt=""> <br/>
+   <div class="stars">
+     <img src="" alt=""> <br/>
      <input type="radio" name="quality" value="2"> Good
    </div>
+   <div class="stars">
+     <img src="" alt=""> <br/>
+     <input type="radio" name="quality" value="3"> Very good
+   </div>
+   <div class="stars">
+     <img src="" alt=""> <br/>
+     <input type="radio" name="quality" value="4"> Perfect
+   </div>
+
  </div>
  
- <p>Do you have any suggestion for us? </p>
- <textarea name=" suggestion" rows="8" cols="40"></textarea>
-  <input type="submit" name="submit" value="Submit Form">
+ <p>Heeft u enige opmerkingen? </p>
+ <textarea name=" feedbacktekst" rows="8" cols="40"></textarea>
+  <input type="submit" name="submit" value="Typ hier uw feedback!">
 </form>
  </div>    
 
