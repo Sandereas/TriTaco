58a085c128490916e5d935572486ae34e02c232a
Sandereas <1173194@student.roc-nijmegen.nl>
2022-07-01 11:28:52 +0200

    3 of 6

--- php/recensies.php
+++ php/recensies.php
@@ -60,15 +60,6 @@ $recensies = $stmt->fetch();
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Feedback</title>
-</head>
-<body>
 <div class="form_box shadow">
  <form method="post" action="recensies.php">
  <div class="heading">
