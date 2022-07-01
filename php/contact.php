1cec2098b7c443eaecbb70107ea71090b80fa914
Sandereas <1173194@student.roc-nijmegen.nl>
2022-07-01 09:05:59 +0200

    1 out of 6 fixed

--- php/contact.php
+++ php/contact.php
@@ -9,7 +9,7 @@ include ('../config/userdata.php');
     <div class = "box">
         <div class = "contacttext">
             <h3> Voor meer informatie kan je terrecht bij 1 van ons </h3>
-    
+        <br> <br>
 
 
         
@@ -21,17 +21,18 @@ include ('../config/userdata.php');
             <th>Email</th>
         </tr>
         <tr>
-        <?php foreach ($datageb as $row) ?> {
+        <?php foreach ($datageb as $row) ?> 
             <td> <?php echo $datageb[0]['username'] ?> </td>
             <td> <?php echo $datageb[0]['email'] ?> </td>
-            <td> <a href= "contactmail.php" class="buttonadmin"> Mail</a> </td>
         </tr>
         <tr>
             <td> <?php echo $datageb[1]['username'] ?> </td>
             <td> <?php echo $datageb[1]['email'] ?> </td>
-            <td> <a href="contactmail.php?userID=<?php echo $row['userID']; ?>" class="buttonupdate"> Send Mail </a> </td>
         </tr>
-        }
+        
+
+        <a href= "contactmail.php" class="buttonadmin"> Send Email</a>
+
     </table>
     </div>
     
--- php/contactmail.php
+++ php/contactmail.php
@@ -1,18 +1,25 @@
 <?php include('../config/header.php'); ?>
 
+<?php if($_SESSION['loggedin'] == true){
+     echo $_SESSION['username'];
+ }
+ else {
+         header("Location: login.php");
+ } ?>
+
 
 <div class="container">
-  <form action="action_page.php">
+  <form action="contact.php">
 
-    <label for="fname">Full name</label>
-    <input type="text" id="fullname" name="fullname" placeholder="Your full name..">
-
-    <label for="lname">User Name</label>
+    <label for="fullname">Full name</label>
+    <input type="text" id="fullname" name="fullname" placeholder="Your full name.."> 
+    <br> <br>
+    <label for="username">User Name</label>
     <input type="text" id="username" name="username" placeholder="Your user name..">
-
-    <label for="lname">Email</label>
+    <br> <br>
+    <label for="email">Email</label>
     <input type="email" id="email" name="Email" placeholder="Your email.">
-
+    <br> <br>
     <label for="Type">Type mail</label>
     <select id="Type" name="Type">
       <option value="Vraag">Vraag</option>
@@ -20,11 +27,13 @@
       <option value="Opmerking">Opmerking</option>
       <option value="Overig">Overig</option>
     </select>
-
-    <label for="subject">Subject</label>
+    <br>
+    <label for="subject"></label>
     <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
 
     <input type="submit" value="Submit">
 
   </form>
 </div>
+
+<?php include('../config/footer.php'); ?>
